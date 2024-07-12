e7bac6d958d5fabe0488d7b2ef9b1df04be7c25a
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-07-13 00:43:27 +0300

    commit

--- src/components/ListView/ListView.jsx
+++ src/components/ListView/ListView.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from "./list-view.module.css";
+import {ShopItem} from "./ShopItem/index.js";
 
 export function ListView({products}) {
   return (
-    <div>
-
+    <div className={styles.container}>
+      {products.map(product => (
+        <ShopItem product={product}></ShopItem>
+      ))}
     </div>
   );
 }
--- src/components/ListView/ShopItem/ShopItem.jsx
+++ src/components/ListView/ShopItem/ShopItem.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import styles from "./shop-item.module.css";
+import listViewStyles from "../../ListView/list-view.module.css";
 
 export function ShopItem({product}) {
   return (
-    <div>
-
-    </div>
+    <article className={[styles.container, listViewStyles.element].join(" ")}>
+      <div className={styles.element}>
+        <img src={product.img} alt={product.name} className={styles.image}/>
+      </div>
+      <div className={styles.element}>
+        <h3>{product.name}</h3>
+      </div>
+      <div className={styles.element}>
+        <span>{product.color}</span>
+      </div>
+      <div className={styles.element}>
+        <span className={styles.price}>${product.price}</span>
+      </div>
+      <div className={styles.element}>
+        <button className={styles["buy-button"]}>Add to cart</button>
+      </div>
+    </article>
   );
 }
