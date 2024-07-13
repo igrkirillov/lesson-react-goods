90f3b883ae9f8a829bee85c07524e57ce6c2a1c6
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-07-13 11:57:49 +0300

    commit

--- src/components/CardsView/CardsView.jsx
+++ src/components/CardsView/CardsView.jsx
@@ -3,11 +3,10 @@ import {ShopCard} from "./ShopCard/index.js";
 import styles from "./cards-view.module.css"
 
 export function CardsView({products}) {
-  let index = 0;
   return (
     <div className={styles.container}>
       {products.map(product => (
-        <ShopCard key={index++} product={product}></ShopCard>
+        <ShopCard key={product.name} product={product}></ShopCard>
       ))}
     </div>
   );
--- src/components/ListView/ListView.jsx
+++ src/components/ListView/ListView.jsx
@@ -3,11 +3,10 @@ import styles from "./list-view.module.css";
 import {ShopItem} from "./ShopItem/index.js";
 
 export function ListView({products}) {
-  let index = 0;
   return (
     <div className={styles.container}>
       {products.map(product => (
-        <ShopItem key={index++} product={product}></ShopItem>
+        <ShopItem key={product.name} product={product}></ShopItem>
       ))}
     </div>
   );
